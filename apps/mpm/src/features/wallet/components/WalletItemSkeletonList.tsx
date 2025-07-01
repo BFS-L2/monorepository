834c57c0861e813ec144f5eb355789bcc9691dd2
Andrew <tfortoka@gmail.com>
2025-07-02 02:22:33 +0300

    Skeleton...

--- apps/mpm/src/features/wallet/components/WalletItemSkeletonList.tsx
+++ apps/mpm/src/features/wallet/components/WalletItemSkeletonList.tsx
@@ -4,20 +4,19 @@ export const WalletItemSkeletonList = () => {
 			{Array.from({ length: 8 }).map((_, i) => (
 				<div
 					key={i}
-					className='className="flex dark:bg-zinc-900" animate-pulse items-center justify-between rounded-lg bg-zinc-100 p-3'
+					className='flex animate-pulse items-center justify-between rounded-lg bg-zinc-100 p-3 dark:bg-zinc-900'
 				>
 					<div className='flex items-center gap-2'>
 						<div className='h-10 w-10 rounded-full bg-zinc-300 dark:bg-zinc-700' />
-
-						<div className='flex flex-col gap-1'>
-							<div className='h-5 w-16 rounded bg-zinc-300 dark:bg-zinc-700' />
-							<div className='h-4 w-24 rounded bg-zinc-200 dark:bg-zinc-600' />
+						<div>
+							<div className='mb-1 h-4 w-16 rounded bg-zinc-300 dark:bg-zinc-700' />
+							<div className='h-3 w-24 rounded bg-zinc-200 dark:bg-zinc-600' />
 						</div>
 					</div>
 
 					<div className='text-right'>
-						<div className='mb-1 h-5 w-20 rounded bg-zinc-300 dark:bg-zinc-700' />
-						<div className='h-4 w-28 rounded bg-zinc-200 dark:bg-zinc-600' />
+						<div className='mb-1 h-4 w-24 rounded bg-zinc-300 dark:bg-zinc-700' />
+						<div className='h-3 w-20 rounded bg-zinc-200 dark:bg-zinc-600' />
 					</div>
 				</div>
 			))}
